7f3e1a4b39b8581a8e146ffeb7404f4ab8c9990b
BuildTools <unconfigured@null.spigotmc.org>
2020-09-23 09:39:39 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+const tesla = require ('./tesla.js');
+var cookieParser = require('cookie-parser');
+const cheerio = require('cheerio');
+var fs = require('fs');
+
+// Adding functions to app()
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Allowing access to static pages
+app.use('/static', express.static('html'))
+app.use('/static', express.static('images'))
+
+const $ = cheerio.load(fs.readFileSync(__dirname + '/html/template.html'));
+
+function updatedHtml(res){
+    res.send($.html())
+}
+
+var called = false
+function tesla_list(token, res){
+    tesla.listcars(token).then(result2 => {
+        if(called == false){
+        for(let index = 0; index < result2.count; index++) {
+           $('div.lu').append(`
+           <div class="li">
+           <p>${result2.response[index].display_name}</p>
+           <img src="https://static-assets.tesla.com/v1/compositor/?model=mx&view=STUD_3QTR_V2&size=700&options=MTX03,COL2-PMNG,WT20&bkba_opt=1&context=design_studio_2.png">
+            </div>
+            <div class="content">
+           <a href="#Wakeup" value="Wakeup">Wakeup</a>
+           <a href="#" value="Unlock Doors">Unlock Doors</a>
+           <a href="#" value="Lock Doors">Lock Doors</a>
+           <a href="#" value="Honk Horn">Honk Horn</a>
+           <a href="#" value="Flash Lights">Flash Lights</a>
+           <a href="#" value="Start HVAC System">Start HVAC System</a>
+           <a href="#" value="Stop HVAC System">Stop HVAC System</a>
+           <a href="#" value="Set Temperature">Set Temperature</a>
+           <a href="#" value="Set Charge Limit">Set Charge Limit</a>
+           <a href="#" value="Set Max Range Charge Limit">Set Max Range Charge Limit</a>
+           <a href="#" value="Set Standard Charge Limit">Set Standard Charge Limit</a>
+           <a href="#" value="Open sun roof">Open sun roof</a>
+           <a href="#" value="Actuate trunk">Actuate trunk</a>
+            </div>`);
+       }
+       return called = true
+    }
+               updatedHtml(res)
+       }
+    )}
+
+app.post("/login", async (req, res) =>{
+    var email = req.body.Email
+    var pass = req.body.Pass
+    if(req.cookies.access_token){
+        res.redirect('/cars')
+    }
+    else{
+    console.log(req)
+    tesla.RenewPassword(email, pass).then(result => {
+        res.redirect('/cars')
+        res.cookie('access_token', `${result.access_token}`) 
+    })
+    .catch(error =>{
+        res.send('User name or password incorrect! Try Again!')
+    }
+    )
+}
+})
+
+app.get("/cars", async (req, res) =>{
+    var token = req.cookies.access_token
+    tesla_list(token, res)
+}
+)
+
+app.listen (3003, () => {
+    console.log('listening on port 3003')
+})
---
+++ tesla.js
@@ -0,0 +1,54 @@
+const axios = require('axios')
+async function RenewPassword (email, pass) {
+    return axios
+        .post('https://owner-api.teslamotors.com/oauth/token', {
+                "grant_type": "password",
+                'client_secret' : 'c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3',
+                'client_id' : '81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384',
+                'email' : `${email}`,
+                'password' : `${pass}`
+    }
+    )
+            .then((res) => {
+                    console.log(`statusCode: ${res.statusCode}`);
+                    return res.data;
+                })
+              .catch((error) => {
+                console.error(error)
+              })
+
+}
+
+async function RenewRefresh (refresh) {
+    return axios
+        .post('https://owner-api.teslamotors.com/oauth/token', {
+                "grant_type": "refresh_token",
+                'client_secret' : 'c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3',
+                'client_id' : '81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384',
+                'refresh_token' : `${refresh}`
+    }
+    )
+            .then((res) => {
+                    console.log(`statusCode: ${res.statusCode}`);
+                    return res.data
+                })
+              .catch((error) => {
+                console.error(error)
+              })
+}
+
+async function listcars (token) {
+    return axios
+        .get('https://owner-api.teslamotors.com/api/1/vehicles', {
+                headers: {'Authorization': `Bearer ${token}`}
+    }
+    )
+            .then((res) => {
+                    console.log(`statusCode: ${res.statusCode}`);
+                    return res.data
+                })
+              .catch((error) => {
+                console.error(error)
+              })
+}
+module.exports = { RenewPassword, RenewRefresh, listcars }
