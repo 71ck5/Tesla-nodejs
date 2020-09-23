4c3513b9d5ea5164bda5328b1e9a23cf5d137bb0
BuildTools <unconfigured@null.spigotmc.org>
2020-09-23 11:35:40 -0500

    Updated port for azure

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const $ = cheerio.load(fs.readFileSync(__dirname + '/public/template.html'));
 function updatedHtml(res){
     res.send($.html())
 }
-
+app.get
 var called = false
 function tesla_list(token, res){
     tesla.listcars(token).then(result2 => {
@@ -81,6 +81,4 @@ app.get("/", (req,res) => {
     res.redirect('/public/')
 })
 
-app.listen (80, () => {
-    console.log('listening on port 80')
-})
+app.listen(process.env.PORT||3000,console.log('3000'))
